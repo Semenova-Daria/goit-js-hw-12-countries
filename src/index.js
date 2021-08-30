eafc84f83915a967c9ceaa4ee0ddd8eb90e3cf2b
Semenova-Daria <g.woman.one@gmail.com>
2021-08-31 00:42:44 +0300

    +markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-import './sass/main.scss';
+import './css/styles.css';
+import { alert, info, success, error } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/BrightTheme.css';
+import debounce from 'lodash.debounce';
+import fetchCountries from './js/fetchCountries';
+import countryCardsTpl from './templates/countryCardsTpl.hbs';
+
+
+
+fetch(`https://restcountries.eu/rest/v2/name/Switzerland`)
+    .then(response => {
+        return (response.json());
+    })
+    .then(name => {
+        console.log(name);
+        const markup = countryCardsTpl(name);
+        console.log(markup);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+
+
+// alert({
+//     text: 'Notice me, senpai!'
+//   });
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,14 @@
+// export default function fetchCountries(searchQuery) {
+// fetch(`https://restcountries.eu/rest/v2/name/Switzerland`)
+//     .then(response => {
+//         return (response.json());
+//     })
+//     .then(name => {
+//         console.log(name);
+//         const markup = countryCardsTpl(Switzerland);
+//         console.log(markup);
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     });
+// };
